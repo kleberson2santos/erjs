4bc13f3f5b4305acfc2b57e42c368f843e9e5662
Kleberson Santos <kleberson2santos@gmail.com>
2021-07-06 21:55:48 -0400

    jsx tambem é uma expressão

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,9 +1,16 @@
 export default function HelloWorld(){
     const name = 'Kleber'
 
+    const title = <h1>Hello, {name}</h1>
+
+
+    if(name==='Kleber'){
+        return <div>
+            <h1 style={{ background: 'red'}}>{ name }</h1>
+        </div>
+    }
+
     return <div>
-        <h1>
-            Hello, { name === 'Kleber'+1 ? 'Dev' : 'Batata'} !!!
-        </h1>
+        {title}
     </div>
 }
