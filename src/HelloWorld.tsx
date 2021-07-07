08a7fc139ec27f275d3a8e874b7bc7d252c28335
Kleberson Santos <kleberson2santos@gmail.com>
2021-07-07 19:28:40 -0400

    a seguranca do jsx

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,17 +1,15 @@
+const maliciousScript =`
+<script>
+    console.log('sent')
+<script>
+`
+
 export default function HelloWorld(){
     const name = 'Kleber'
 
     const title = <h1>Hello, {name}</h1>
 
-    const index = 0
-
-    if(name==='Kleber'){
-        return <div>
-            <h1 tabIndex={index} style={{ background: 'red'}}>{ name }</h1>
-        </div>
-    }
-
     return <div>
-        {title}
+        {maliciousScript}
     </div>
 }
