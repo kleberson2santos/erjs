bae2f445bae947e5e10bda491e9924f7af6ad302
Kleberson Santos <kleberson2santos@gmail.com>
2021-07-23 19:46:39 -0400

    estados em Class Component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,32 @@ import { Component } from 'react';
 
 interface ButtonProps {
   onClick?: () => any;
+  initializeClicked?: boolean;
 }
 
-class Button extends Component<ButtonProps> {
+interface ButtonState {
+  alreadyCliked: boolean;
+}
+
+class Button extends Component<ButtonProps, ButtonState> {
+  constructor(props: ButtonProps) {
+    super(props);
+    this.state = { alreadyCliked: !!props.initializeClicked };
+  }
+
   render() {
-    // console.log(this.props);
-    return <button onClick={this.props.onClick}>{this.props.children}</button>;
+    console.log(this.state);
+    return (
+      <button
+        onClick={() => {
+          this.setState({ alreadyCliked: true });
+          this.props.onClick?.call([]);
+        }}
+        disabled={this.state.alreadyCliked}
+      >
+        {this.props.children}
+      </button>
+    );
   }
 }
 
