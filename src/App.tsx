3f5b0523ef4d386700abed0d1c18fe1fc2d1cbbd
Kleberson Santos <kleberson2santos@gmail.com>
2021-07-29 22:01:39 -0400

    inteligação com formulários

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import './App.css';
+import Form from './components/Form';
 
 function App() {
-  console.log('renderizou app');
-
-  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    // e.preventDefault();
-  }
-
   return (
     <div className='App'>
       <header className='App-header'>
-        <form action='/test'>
-          <button type='submit' onClick={handleClick}>
-            Clique em mim
-          </button>
-        </form>
+        <Form />
       </header>
     </div>
   );
