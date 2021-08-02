dd21f4f7654c81a782326063025153174c4fd1a0
Kleberson Santos <kleberson2santos@gmail.com>
2021-08-01 20:44:56 -0400

    ciclos de vida com useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const names = ['John', 'Marie', 'Phill', 'John'];
+  const [names, setNames] = useState([
+    'John',
+    'Marie',
+    'Phill',
+    'John',
+    'Alex',
+  ]);
+
+  useEffect(() => {
+    console.log('Mounting App');
+
+    return () => {
+      console.log('Unmonting App');
+    };
+  }, []);
+
   return (
     <div className='App'>
       <header className='App-header'>
@@ -10,6 +27,13 @@ function App() {
             <li key={index}>{name}</li>
           ))}
         </ul>
+        <button
+          onClick={() => {
+            setNames([...names, 'jeremias']);
+          }}
+        >
+          adicionar jeremias
+        </button>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+function MyComponent() {
+  const [canViewApp, setCanViewApp] = useState(true);
+
+  return (
+    <>
+      {canViewApp && <App />}
+      <button
+        onClick={() => {
+          setCanViewApp(!canViewApp);
+        }}
+      >
+        toggle
+      </button>
+    </>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
